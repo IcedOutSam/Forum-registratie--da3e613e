e80d32bb5997d3148417023958fc11f5a46eab0c
Sam <2102926@talnet.nl>
2020-04-28 19:12:50 +0200

    Uitwerking van de exercise

---
+++ forum_registratie.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Same song</title>
+  </head>
+  <body>
+
+    <form action="http://localhost/bit/PHP%20Web/Web%20uitgebreid/Forum-registratie--da3e613e/registratie_handler.php" method="post" class="form-example">
+  <div class="form-example">
+    <label for="name">Enter your name: </label>
+    <input type="text" name="name" id="name" required>
+  </div>
+  <div class="form-example">
+    <label for="email">Enter your email: </label>
+    <input type="email" name="email" id="email" required>
+  </div>
+  <div class="form-example">
+    <label for="age">Enter your age: </label>
+    <input type="text" name="age" id="age" required>
+  </div>
+  <div class="form-example">
+    <input type="submit" value="Submit">
+  </div>
+
+</form>
+  </body>
+</html>
+<?php
+
+?>
---
+++ registratie_handler.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <p>Uw naam is: </p>   <?php
+echo $_POST["name"];
+?>
+  <br>
+  <p>Uw email is: </p>   <?php
+echo $_POST["email"];
+?>
+  <br>
+  <p>Uw leeftijd is: </p>   <?php
+echo $_POST["age"];
+?>
+
+  </body>
+</html>
